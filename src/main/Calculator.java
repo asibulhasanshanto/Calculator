235e936bb4b8b78b76f2aed810be63258fb2cd2f
Md. Mehedi hasan <mehedi.rucse@gmail.com>
2019-04-03 10:25:04 +0600

    Division Method was added in Calculator Class

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -3,7 +3,7 @@ package main;
 public class Calculator {
     double divisionMethod(double first , double second){
         double result = first/second;
-        return double;
+        return result;
     }
 }
 
