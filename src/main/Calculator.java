cda9b10d08ce6e5a5fb693eca1c2cc01d7a1633b
Asibul Hasan <asibulhasan.rucse.18@gmail.com>
2019-04-03 10:29:53 +0600

    addition method added

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,4 +1,18 @@
 package main;
 
-public class Calculator {
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        double firstValue = Double.parseDouble(args[0]);
+        double secondValue = Double.parseDouble(args[2]);
+
+        
+    }
+    double additionMethod(double first,double second)
+    {
+        double result;
+        result = first + second ;
+        return result;
+    }
 }
