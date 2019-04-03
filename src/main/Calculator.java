48bace409c7476d7db543601b282a71aa3c2fb65
Md Alamin <alamin.cse.ru2018@gmail.com>
2019-04-03 10:24:06 +0600

    Multiple method added and return result

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -3,7 +3,7 @@ package main;
 public class Calculator {
 	public double MultipleMethod(double first,double second)
 	{
-		double result = first + second;
+		double result = first * second;
 		return result;
 	}
 }
