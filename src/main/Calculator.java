bf7e8ec7d05e8e57d94f9e643d08ceb9482ff211
Md Alamin <alamin.cse.ru2018@gmail.com>
2019-04-03 10:34:06 +0600

    Multiplication method has been added

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -1,7 +1,7 @@
 package main;
 
 public class Calculator {
-	public double MultipleMethod(double first,double second)
+	public double MultiplicationMethod(double first,double second)
 	{
 		double result = first * second;
 		return result;
