bc4100dccfb8546f37048cbb54bd89ea22d27c99
Md. Mehedi hasan <mehedi.rucse@gmail.com>
2019-04-03 12:42:34 +0600

    Final Commitment of Division class

--- src/main/Calculator.java
+++ src/main/Calculator.java
@@ -2,11 +2,18 @@ package main;
 
 public class Calculator
 {
+    public static void main(String[] args)
+    {
+        double firstValue = Double.parseDouble(args[0]);
+        double secondValue = Double.parseDouble(args[2]);
+
+        
+    }
+    
     double divisionMethod(double first , double second)
     {
-        double result = first / second;
+        double result;
+        result = first / second ;
         return result;
     }
 }
-
-
